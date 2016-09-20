e806561001c11f6a41e84e2a4c797eb7f3a55689
Gajendra Chauhan <gschauhan.007@gmail.com>
2016-09-21 03:16:30 +0530

    [Mock-Testing]

---
+++ EmployeeDetailsTest/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeDetailsDAL.EmployeeInfoClasses;
+using Moq;
+
+namespace EmployeeDetailsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IEmployeeProcess employeeProcess;
+
+        public UnitTest1()
+        {
+            employeeProcess = new EmployeeProcess();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Employees employee = new Employees
+            {
+                FirstName = "Akriti",
+                LastName = "Mehara",
+                DateOfBirth = DateTime.Now.AddDays(-5000),
+                Designation = "Associate Software Engineer",
+                City = "Bangalore",
+                Gender = "Female"
+            };
+            Mock<IEmployeeProcess> emp = new Mock<IEmployeeProcess>();
+            emp.Setup(x => x.AddEmployee(employee)).Returns(true);
+
+            Assert.AreEqual(emp.Object.AddEmployee(employee), true);
+        }
+    }
+}
